2e8f63e517ca2ffad282cfa0708f2ce26fdaff5c
Void User <nlantz@pm.me>
2019-10-14 10:56:32 -0600

    Added waveformn.rkt, migrated relevant code

--- src/synth.rkt
+++ src/synth.rkt
@@ -29,30 +29,6 @@
 			  (P8 . 1200)))])
     (cdr (assoc name intervals))))
 
-;; Note sin is also an available shapes
-
-(define (sine x)
-  (sin (* 2 pi x)))
-
-(define (fractional x)
-  (- x (floor x)))
-
-(define (square x)
-  (sub1 (* 2 (round (fractional x)))))
-
-(define (sawtooth x)
-  (sub1 (* 2 (fractional x))))
-
-;; TODO: Remove `if`
-(define (triangle x)
-  (let* ([p (fractional x)])
-    (if (< p 0.5)
-	(sub1 (* p 4))
-	(add1 (- (* (- p 0.5) 4))))))
-
-(define (flatline x)
-  0)
-
 (define (pitch frequency wave)
   (lambda (t)
     (wave (* frequency t))))
